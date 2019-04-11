f700b7d4472fe59e35536b68a8e0d9aad3b8009a
Dean Smith <smith.dean@talk21.com>
2019-04-11 13:11:38 +0100

    Fix route for appointment question

--- app/routes.js
+++ app/routes.js
@@ -3,7 +3,7 @@ const router = express.Router()
 
 // Add your routes here - above the module.exports line
 
-router.post('/name', function (req, res) {
+router.post('/name-single-field', function (req, res) {
   // Get the answer from session data
   // The name between the quotes is the same as the 'name' attribute on the input elements
   // However in JavaScript we can't use hyphens in variable names
